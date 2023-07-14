de6da0f6d1a2c9b9b5ec8952fe4f158274cff5ee
Lautaro <laumol159@gmail.com>
2023-07-13 21:36:07 -0300

    page empleados

--- BlazorCrud.Client/Program.cs
+++ BlazorCrud.Client/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using BlazorCrud.Client.Services;
+using CurrieTechnologies.Razor.SweetAlert2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5050") });
 builder.Services.AddScoped<iDepartamentoService,DepartamentoService>();
 builder.Services.AddScoped<IEmpleadoService,EmpleadoService>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
